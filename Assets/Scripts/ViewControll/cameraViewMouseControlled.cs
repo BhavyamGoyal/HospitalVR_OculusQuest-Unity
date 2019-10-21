e68518b00c2d5cd0c02c178f65cf6981498dc72e
Bhavyam Goyal <bhavyam.kr.goyal@gmail.com>
2019-10-22 03:31:08 +0530

    cursor inside game screen

--- Assets/Scripts/ViewControll/cameraViewMouseControlled.cs
+++ Assets/Scripts/ViewControll/cameraViewMouseControlled.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraViewMouseControlled : MonoBehaviour
-{
+public class cameraViewMouseControlled : MonoBehaviour {
     public float speedH = 2.0f;
     public float speedV = 2.0f;
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
 
+    public void Start () {
+       
+    }
     // Update is called once per frame
-    void Update()
-    {
+    void Update () {
+        Screen.lockCursor = true;
+        Cursor.visible = true;
+         yaw += speedH * Input.GetAxis ("Mouse X");
+        pitch -= speedV * Input.GetAxis ("Mouse Y");
 
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
-
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        transform.eulerAngles = new Vector3 (pitch, yaw, 0.0f);
 
     }
 }
